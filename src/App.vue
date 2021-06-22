db7e7448bf1d293334ae3bdae6fe15dff6904368
ronnapon <ronnapon.pra@gmail.com>
2021-06-22 23:41:47 +0700

    Loging Screen

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
   <div id="app">
-    <input type="text" v-model="message" />
-    {{ message }}
-    <div v-if="show == true">Vue.js.EP1</div>
-    <div v-else>Vue.js EP2</div>
-    <!-- valiable, index -->
-    <div v-for="(animal, i) in animals" :key="i">
-      {{ animal }}
-    </div>
-    <button v-on:click="submit()">click</button>
-    {{ message }}
+    <table>
+      <tr>
+        <h1 v-if="level == 1">Register</h1>
+      </tr>
+      <tr>
+        <td width>Username</td>
+        <td><input type="text" v-model="username" /></td>
+        {{
+          username
+        }}
+      </tr>
+      <tr>
+        <td>Password</td>
+        <td><input type="password" v-model="password" /></td>
+        {{
+          password
+        }}
+      </tr>
+      <br />
+      <tr>
+        <button v-on:click="submit()">Submit</button>
+        <button v-on:click="clear()">Clear</button>
+        <div v-if="show == true">บันทึกเรียบร้อยแล้ว</div>
+      </tr>
+    </table>
+    <table>
+      <br /><br />
+      <tr>
+        <td>
+          <div v-if="show == true">
+            ข้อมูลสัตว์
+            <div v-for="(animal, i) in animals" :key="i">
+              {{ animal }}
+            </div>
+          </div>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -18,15 +46,24 @@ export default {
   name: "App",
   data() {
     return {
+      level: "1",
+      username: "",
+      password: "",
       message: "",
-      show: true,
+      show: false,
       animals: ["cat", "dog", "ant"],
     };
   },
   methods: {
     submit() {
-      this.message = "Make Web Bkk";
-      //console.log("Make Web BigThaiFlute");
+      this.message = "บันทึกเรียบร้อยแล้ว";
+      this.show = true;
+    },
+    clear() {
+      this.username = "";
+      this.password = "";
+      this.message = "";
+      this.show = false;
     },
   },
 };
